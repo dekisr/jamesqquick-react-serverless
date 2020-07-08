a2bc7844a641cd338ff02407623671befa57e451
Hideki J <aledinai@gmail.com>
2020-07-08 19:57:55 -0300

    Add Section 02 - Styling with Styled Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import './App.css'
 import Home from './pages/Home'
 import Game from './pages/Game'
 import HighScores from './pages/HighScores'
 import GameOver from './pages/GameOver'
 import Navbar from './components/Navbar'
+import { Container } from './styled/Container'
+import { Main } from './styled/Main'
+import Global from './styled/Global'
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/game">
-          <Game />
-        </Route>
-        <Route path="/highscores">
-          <HighScores />
-        </Route>
-        <Route path="/gameover">
-          <GameOver />
-        </Route>
-      </Switch>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/game">
+              <Game />
+            </Route>
+            <Route path="/highscores">
+              <HighScores />
+            </Route>
+            <Route path="/gameover">
+              <GameOver />
+            </Route>
+          </Switch>
+        </Container>
+      </Main>
     </Router>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+  StyledNavbar,
+  StyledNavBrand,
+  StyledNavItems,
+  StyledLink,
+} from '../styled/Navbar'
+import { Accent } from '../styled/Random'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <StyledNavbar>
+      <StyledNavBrand>
         <Link to="/">
-          Learn.Build.<span>Type.</span>
+          Learn.Build.<Accent>Type.</Accent>
         </Link>
-      </div>
-      <ul>
+      </StyledNavBrand>
+      <StyledNavItems>
         <li>
-          <Link to="/">Home</Link>
+          <StyledLink to="/">Home</StyledLink>
         </li>
         <li>
-          <Link to="/highscores">High Scores</Link>
+          <StyledLink to="/highscores">High Scores</StyledLink>
         </li>
-      </ul>
-    </nav>
+      </StyledNavItems>
+    </StyledNavbar>
   )
 }
 
