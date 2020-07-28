45b9348a2beb96c13be4112df564add4b6832648
Hideki J <aledinai@gmail.com>
2020-07-28 18:12:37 -0300

    Add Section 03 - Game Functionality with React Hooks

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   StyledGame,
   StyledScore,
@@ -8,14 +9,79 @@ import {
 import { Strong } from '../styled/Random'
 
 const Game = () => {
+  const MAX_SECONDS = 50
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  const [currentCharacter, setCurrentCharacter] = useState('')
+  const [score, setScore] = useState(0)
+  const [ms, setMs] = useState(0)
+  const [seconds, setSeconds] = useState(MAX_SECONDS)
+  const history = useHistory()
+
+  const updateTime = useCallback((startTime) => {
+    const endTime = new Date()
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString()
+    const formattedMSString = ('0000' + msPassedStr).slice(-5)
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1
+    const updatedMs =
+      1000 - parseInt(formattedMSString.substring(formattedMSString.length - 3))
+    setSeconds(addLeadingZeros(updatedSeconds, 2))
+    setMs(addLeadingZeros(updatedMs, 3))
+  }, [])
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = ''
+    for (let i = 0; i < length; i++) {
+      zeros += 0
+    }
+    return (zeros + num).slice(-length)
+  }
+
+  const keyUpHandler = useCallback((e) => {
+    if (e.key === currentCharacter) {
+      setScore((prevScore) => prevScore + 1)
+    } else {
+      score && setScore((prevScore) => prevScore - 1)
+    }
+    setRandomCharacter()
+  }, [currentCharacter, score])
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36)
+    setCurrentCharacter(characters[randomInt])
+  }
+
+  useEffect(() => {
+    setRandomCharacter()
+    const currentTime = new Date()
+    const interval = setInterval(() => updateTime(currentTime), 1)
+    return () => clearInterval(interval)
+  }, [updateTime])
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push('/gameover')
+    }
+  }, [seconds, history])
+
+  useEffect(() => {
+    document.addEventListener('keyup', keyUpHandler)
+    return () => {
+      document.removeEventListener('keyup', keyUpHandler)
+    }
+  }, [keyUpHandler])
+
   return (
     <StyledGame>
       <StyledScore>
-        Score:<Strong>0</Strong>
+        Score:<Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>
-        Time: <Strong>00:000</Strong>
+        Time:{' '}
+        <Strong>
+          {seconds}:{ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   )
