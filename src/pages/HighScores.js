afdd00733db1c623abe8c68e55b16ab24c28cf8d
Hideki J <aledinai@gmail.com>
2020-07-31 21:51:40 -0300

    Add Section 06 - saveHighScore function / display high scores component

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ScoresList, ScoreLI } from '../styled/HighScores'
 
 const HighScores = () => {
+  const [highScores, setHighScores] = useState([])
+  useEffect(() => {
+    console.log('getting high scores...')
+    const loadHighScores = async () => {
+      try {
+        const response = await fetch('/.netlify/functions/getHighScores')
+        const scores = await response.json()
+        setHighScores(scores)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    loadHighScores()
+  }, [])
   return (
     <div>
       <h1>HighScores</h1>
+      <ScoresList>
+        {highScores.map((score) => (
+          <ScoreLI key={score.id}>
+            {score.fields.name} - {score.fields.score}
+          </ScoreLI>
+        ))}
+      </ScoresList>
     </div>
   )
 }
