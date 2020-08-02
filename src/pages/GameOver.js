92d353babdfcd495f084ca016e9aaf64dbc23ee8
Hideki J <aledinai@gmail.com>
2020-08-02 18:16:11 -0300

    Update Section 06 - Save high score on game over

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useScore } from '../contexts/ScoreContext'
 import { useHistory } from 'react-router-dom'
 import { StyledLink } from '../styled/Navbar'
+import { StyledCharacter } from '../styled/Game'
 
 const GameOver = () => {
   const [score] = useScore()
+  const [scoreMessage, setScoreMessage] = useState('')
   const history = useHistory()
 
   if (score === -1) {
     history.push('/')
   }
 
+  useEffect(() => {
+    const saveHighScore = async () => {
+      try {
+        const options = {
+          method: 'POST',
+          body: JSON.stringify({
+            name: 'HIGH?',
+            score,
+          }),
+        }
+        const response = await fetch(
+          '/.netlify/functions/saveHighScore',
+          options
+        )
+        const data = await response.json()
+        if (data.id) {
+          setScoreMessage('Congrats! You got a high score!')
+        } else {
+          setScoreMessage('Sorry, not a high score...')
+        }
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    saveHighScore()
+  }, [score])
+
   return (
     <div>
       <h1>GameOver</h1>
-      <p>{score}</p>
+      <StyledCharacter>{score}</StyledCharacter>
+      <p>{scoreMessage}</p>
       <StyledLink to="/">Go Home</StyledLink>
       <StyledLink to="/game">Play Again</StyledLink>
     </div>
