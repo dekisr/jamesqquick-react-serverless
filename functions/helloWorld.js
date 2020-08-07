b4ffaa98af109b1b63104327958b5582d7dd5d71
Hideki J <aledinai@gmail.com>
2020-08-07 01:11:59 -0300

    Cors Test

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,6 +1,10 @@
 exports.handler = async (event, context, callback) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify({
       msg: 'Hello World!',
     }),
