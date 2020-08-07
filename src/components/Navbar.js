f516d2ce58bdfbf8fccffe677c3daf82e592572a
Hideki J <aledinai@gmail.com>
2020-08-07 01:14:31 -0300

    Cors Test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 
-import useTheme from '../hooks/UseTheme'
+// import useTheme from '../hooks/UseTheme'
 
 import {
   StyledNavbar,
